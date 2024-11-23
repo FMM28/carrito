c83afd1030c49a8cb87983c02e1ac03ff80e3d16
anahilozano09 <anahilozano319@aragon.unam.mx>
2024-11-23 13:16:41 -0600

    Modelo Usuarios

--- models/Usuarios.js
+++ models/Usuarios.js
@@ -9,20 +9,34 @@ export const Usuarios = db.define(
             autoIncrement: true,
             primaryKey: true,
         },
+        username:{
+            type:Sequelize.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        password:{
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
         nombre:{
             type:Sequelize.STRING,
+            allowNull: false,
         },
         ap_paterno:{
             type: Sequelize.STRING,
+            allowNull: false,
         },
         ap_materno:{
             type: Sequelize.STRING,
         },
         email:{
             type: Sequelize.STRING,
+            allowNull: false,
+            unique: true,
         },
-        telefono:{
-            type: Sequelize.STRING
+        rol:{
+            type: Sequelize.ENUM('user','admin'),
+            defaultValue:'user',
         }        
     },
     {timestamps:false}
