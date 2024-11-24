458b4b82f3f2c9780f3261bbcf81d836da2aa266
FMM28 <marquez.maya.francisco.28@gmail.com>
2024-11-23 18:45:45 -0600

    rutas

---
+++ routes/compras_router.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import {} from '../controllers/compras/comprasController.js'
+
+const router = express.Router()
+
+router.get('/',inicio)
+
+export default router
---
+++ routes/login_router.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import {} from '../controllers/login/loginController.js'
+
+const router = express.Router()
+
+router.get('/',inicio)
+
+export default router
