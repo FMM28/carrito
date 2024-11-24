8cd1a3eaecbe5b0e2fd6148d07921b7e619f30c8
Mikasa500 <josue.hernandez.rosas246@gmail.com>
2024-11-23 20:43:45 -0600

    modified:   controllers/compras/comprasController.js

---
+++ controllers/compras/comprasController.js
@@ -0,0 +1,10 @@
+import Ticket from "../../models/Ticket";
+import TicketProducto from "../../models/TicketProducto";
+import Pago from "../../models/Pago";
+
+
+const accionMostrarCompras = async (req,res) =>{
+
+}
+
+
